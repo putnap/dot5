af927ce5c683f42be7dfeb860a0dcfebeea974b5
putnap <putna.paulius@gmail.com>
2014-11-22 13:44:45 +0200

    Problem: server wasn't configured to listen async
    Server opens connection on random port, also added random port and name
    configuration.

--- dot5/dot5-server/Program.cs
+++ dot5/dot5-server/Program.cs
@@ -5,22 +5,61 @@ namespace dot5_server
 {
     class Program
     {
+        static string defaultIp = "127.0.0.1";
+        static string defaultServerName = "server";
+
         static void Main(string[] args)
         {
+            string newIP;
+            string serverName = "myName";
+            if (args.Length > 0)
+            {
+                newIP = args[0];
+            }
+            else
+            {
+                Console.Write("Please enter a server ip: ");
+                newIP = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(newIP))
+                {
+                    newIP = string.Format("tcp://{0}", newIP);
+                }
+                else
+                    newIP = defaultIp;
+            }
+
+            Console.Write("Please enter a server name: ");
+            serverName = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(serverName))
+                serverName = defaultServerName;
+
+            Random rand = new Random();
+            int port = 3000 + rand.Next(4000);
+            string serverBinding = string.Format("tcp://{0}:{1}", newIP, port);
+
             using (var ctx = NetMQContext.Create())
             {
-                using (var server = ctx.CreateResponseSocket())
+                using (var server = ctx.CreateDealerSocket())
                 {
-                    server.Bind("tcp://127.0.0.1:5556");
+                    server.Options.SendTimeout = TimeSpan.FromMilliseconds(500);
+                    server.Bind(serverBinding);
+                    Console.WriteLine(string.Format("Server bound on {0}", serverBinding));
 
                     while (true)
                     {
                         var message = server.ReceiveString();
-                        Console.WriteLine("From Client: {0}", message);
-                        server.Send("Hello,");
+                        if (!string.IsNullOrEmpty(message))
+                        {
+                            server.Send(string.Format("{0} {1}", serverName, newIP));
+                            Console.WriteLine("From Client: {0}", message);
+                            break;
+                        }
                     }
                 }
             }
+            Console.ReadLine();
         }
     }
 }
